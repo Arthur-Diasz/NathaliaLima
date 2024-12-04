d3eb875e822be958a7f2b8d156b7429732b9c0cc
Arthur-Diasz <cdp1821@gmail.com>
2024-12-04 08:17:57 -0300

    Avaliações

--- js/script.js
+++ js/script.js
@@ -5,3 +5,30 @@ document.addEventListener('DOMContentLoaded', function () {
         once: false,
     });
 });
+
+
+const testimonialsSwiper = new Swiper('.testimonials-swiper', {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+        768: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 3,
+        }
+    }
+});
